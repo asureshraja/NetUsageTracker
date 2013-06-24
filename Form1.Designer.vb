a380d369d970a2f8fb8b4cb2dc232c4144139448
suresh raja <sureshraja316@gmail.com>
2013-06-24 20:34:58 +0530

    Application Initialized

---
+++ Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox
+        Me.taskbar = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 164)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(66, 34)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Reset"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 241)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(19, 18)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        Me.RichTextBox1.Visible = False
+        '
+        'taskbar
+        '
+        Me.taskbar.BalloonTipTitle = "BandWidth Monitor"
+        Me.taskbar.Icon = CType(resources.GetObject("taskbar.Icon"), System.Drawing.Icon)
+        Me.taskbar.Visible = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(37, 241)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(18, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(84, 164)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(179, 34)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "End"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(59, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 15)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(59, 111)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 15)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(27, 20)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(197, 15)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Bandwidth used at current session:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(27, 86)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(128, 15)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Total Bandwidth used:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(271, 210)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Bandwidth Monitor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents taskbar As System.Windows.Forms.NotifyIcon
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,163 @@
+﻿Public Class Form1
+    Dim a(0 To 1000000) As Char
+    Dim b(0 To 1000000) As Char
+    Dim c As Boolean
+    Dim k As Long
+    Dim num As Double
+    Dim temp As Double
+    Dim fnum As Double
+    Dim temper As Double
+    Dim bw As Double
+    Dim fbw As Double
+    Dim unit As String
+    Dim funit As String
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles Me.FormClosing
+
+        Me.Visible = False
+        e.Cancel = True
+        taskbar.Visible = True
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        unit = "bytes"
+        funit = "bytes"
+      
+        c = True
+        taskbar.Visible = True
+        Label1.Text = ""
+        Timer1.Interval = 1000
+        TextBox1.Text = GetSetting("bndwidthmon", "final", "lastsess", "")
+        RichTextBox1.Text = ""
+        Dim clsProcess As New System.Diagnostics.Process()
+        clsProcess.StartInfo.UseShellExecute = False
+        clsProcess.StartInfo.RedirectStandardOutput = True
+        clsProcess.StartInfo.RedirectStandardError = True
+        clsProcess.StartInfo.FileName = "netstat"
+        clsProcess.StartInfo.Arguments = "-e"
+        clsProcess.StartInfo.CreateNoWindow = True
+        clsProcess.Start()
+        While (clsProcess.HasExited = False)
+            Dim sLine As String = clsProcess.StandardOutput.ReadToEnd
+            Me.RichTextBox1.Text &= sLine & vbCrLf
+            Exit While
+            Application.DoEvents()
+        End While
+
+        num = Double.Parse(OnlyNumbers(RichTextBox1.Text))
+        temp = num
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        fnum = 0
+        SaveSetting("bndwidthmon", "final", "lastsess", fnum)
+        End
+
+    End Sub
+    Function OnlyNumbers(ByVal Str As String) As String
+        Dim i As Long
+        a = Str.ToCharArray
+        k = 0
+
+        For i = 0 To Len(Str)
+
+            If (Asc(a(i)) > 47 And Asc(a(i)) < 58) Then
+                b(k) = a(i)
+                k = k + 1
+                If (Asc(a(i + 1)) < 47 Or Asc(a(i)) > 57) Then
+                    Exit For
+                End If
+            End If
+        Next
+        OnlyNumbers = New String(b)
+
+    End Function
+
+    Private Sub taskbar_BalloonTipClosed(ByVal sender As Object, ByVal e As System.EventArgs) Handles taskbar.BalloonTipClosed
+        c = True
+    End Sub
+
+    Private Sub taskbar_BalloonTipShown(ByVal sender As Object, ByVal e As System.EventArgs) Handles taskbar.BalloonTipShown
+
+        c = False
+
+    End Sub
+
+    Private Sub nfi_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles taskbar.MouseClick
+
+        Me.Show()
+    End Sub
+
+   
+
+    Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        RichTextBox1.Text = ""
+        Dim clsProcess As New System.Diagnostics.Process()
+        clsProcess.StartInfo.UseShellExecute = False
+        clsProcess.StartInfo.RedirectStandardOutput = True
+        clsProcess.StartInfo.RedirectStandardError = True
+        clsProcess.StartInfo.FileName = "netstat"
+        clsProcess.StartInfo.Arguments = "-e"
+        clsProcess.StartInfo.CreateNoWindow = True
+        clsProcess.Start()
+        While (clsProcess.HasExited = False)
+            Dim sLine As String = clsProcess.StandardOutput.ReadToEnd
+            Me.RichTextBox1.Text &= sLine & vbCrLf
+            Exit While
+            Application.DoEvents()
+        End While
+
+        num = Double.Parse(OnlyNumbers(RichTextBox1.Text))
+        num = num - temp
+        temper = num
+        fnum = temper + Val(TextBox1.Text)
+        SaveSetting("bndwidthmon", "final", "lastsess", fnum)
+        bw = temper
+        fbw = fnum
+        If (temper > Math.Pow(2, 10) And temper < Math.Pow(2, 20)) Then
+            bw = bw / 1024
+            unit = "KB"
+        ElseIf (temper > Math.Pow(2, 20) And temper < Math.Pow(2, 30)) Then
+            bw = bw / 1024
+            bw = bw / 1024
+            unit = "MB"
+        ElseIf (temper > Math.Pow(2, 30) And temper < Math.Pow(2, 40)) Then
+            bw = bw / 1024
+            bw = bw / 1024
+            bw = bw / 1024
+            unit = "GB"
+        End If
+        If (fnum > Math.Pow(2, 10) And fnum < Math.Pow(2, 20)) Then
+            fbw = fbw / 1024
+            funit = "KB"
+        ElseIf (fnum > Math.Pow(2, 20) And fnum < Math.Pow(2, 30)) Then
+            fbw = fbw / 1024
+            fbw = fbw / 1024
+            funit = "MB"
+        ElseIf (fnum > Math.Pow(2, 30) And fnum < Math.Pow(2, 40)) Then
+            fbw = fbw / 1024
+            fbw = fbw / 1024
+            fbw = fbw / 1024
+            funit = "GB"
+        End If
+        Label1.Text = Math.Round(bw, 3) & " " & unit
+        Label2.Text = Math.Round(fbw, 3) & " " & funit
+    End Sub
+
+  
+   
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+
+
+    Private Sub taskbar_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles taskbar.MouseMove
+        If (c = True) Then
+            taskbar.BalloonTipText = "Total used: " & Label2.Text & Chr(13) & "session BW: " & Label1.Text
+            taskbar.ShowBalloonTip(3)
+        End If
+    End Sub
+
+End Class
